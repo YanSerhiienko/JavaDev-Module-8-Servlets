deac5f9586a661e73161ba4f89767d6ee4f08c2d
Yan Serhiienko <yanserhiienko@gmail.com>
2023-05-23 19:47:51 +0300

    v2

--- src/main/java/homework/TimezoneValidateFilter.java
+++ src/main/java/homework/TimezoneValidateFilter.java
@@ -10,20 +10,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-@WebFilter(value = "/time*")
+@WebFilter(urlPatterns = "/time")
 public class TimezoneValidateFilter extends HttpFilter {
     ArrayList<String> timeZones = new ArrayList<>(Arrays.asList(
             "UTC-12", "UTC-10", "UTC-9", "UTC-8", "UTC-7",
             "UTC-6", "UTC-5", "UTC-4", "UTC-3", "UTC-2",
-            "UTC-1", "UTC+1", "UTC+2", "UTC+3", "UTC+4",
-            "UTC+5", "UTC+6", "UTC+7", "UTC+8", "UTC+9",
-            "UTC+10", "UTC+11", "UTC+12", "UTC+13", "UTC+14"));
+            "UTC-1", "UTC 1", "UTC 2", "UTC 3", "UTC 4",
+            "UTC 5", "UTC 6", "UTC 7", "UTC 8", "UTC 9",
+            "UTC 10", "UTC 11", "UTC 12", "UTC 13", "UTC 14"));
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         String timezone = req.getParameter("timezone");
 
-        if (timeZones.contains(timezone)) {
+        if (timezone == null || timeZones.contains(timezone)) {
             chain.doFilter(req, res);
         } else  {
             res.setStatus(400);
